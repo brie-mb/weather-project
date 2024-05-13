fb3ac84e35f9d3da32c8fc1c76728d216a79bd03
brie-mb <168968666+brie-mb@users.noreply.github.com>
2024-05-13 10:12:35 +0200

    Updated index.js
    updated weather description, humidity, wind speed, to live data

--- index.js
+++ index.js
@@ -1,40 +1,28 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let currentDay = days[now.getDay()];
-let currentHour = now.getHours();
-let currentMin = now.getMinutes();
-if (currentHour < 10) {
-  currentHour = `0${currentHour}`;
+function displayWeather(response) {
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = response.data.city;
+  let currentTemperature = document.querySelector("#current-temperature-value");
+  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
+  let currentDescription = document.querySelector("#description");
+  currentDescription.innerHTML = response.data.condition.description;
+  let currentHumidity = document.querySelector("#current-humidity");
+  currentHumidity.innerHTML = `${response.data.temperature.humidity}%`;
+  let currentWind = document.querySelector("#wind-speed");
+  currentWind.innerHTML = `${response.data.wind.speed}km/hr`;
 }
-if (currentMin < 10) {
-  currentMin = `0${currentMin}`;
+function searchCity(city) {
+  let apiKey = `1ffb00o632b81060b5933b1eeta0394d`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayWeather);
 }
-let Details = document.querySelector(".current-details");
-Details.innerHTML = `${currentDay} ${currentHour}:${currentMin}, moderate rain <br />Humidity: <strong>87%</strong>, Wind: <strong>7.2km/h</strong>`;
+
+function searchEngine(event) {
+  event.preventDefault();
+  let searchData = document.querySelector("#search-input");
+  searchCity(searchData.value);
+}
 
 let search = document.querySelector(".search-form");
 search.addEventListener("submit", searchEngine);
 
-function displayTemperature(response) {
-  let currentCity = document.querySelector("#current-city");
-  let currentTemperature = document.querySelector("#current-temperature-value");
-  currentCity.innerHTML = response.data.city;
-  currentTemperature.innerHTML = Math.round(response.data.temperature.current);
-}
-function searchEngine(event) {
-  event.preventDefault();
-  let searchData = document.querySelector("#search-input");
-  let city = searchData.value;
-
-  let apiKey = `1ffb00o632b81060b5933b1eeta0394d`;
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(displayTemperature);
-}
+searchCity("Johannesburg");

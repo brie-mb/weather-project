a1ea5232606370aec2868be6dd3e5cf24d53a31c
brie-mb <168968666+brie-mb@users.noreply.github.com>
2024-05-13 10:27:52 +0200

    Updated time, day and weather icon
    Updated time, day and weather icon, to pull from API

--- index.js
+++ index.js
@@ -9,6 +9,29 @@ function displayWeather(response) {
   currentHumidity.innerHTML = `${response.data.temperature.humidity}%`;
   let currentWind = document.querySelector("#wind-speed");
   currentWind.innerHTML = `${response.data.wind.speed}km/hr`;
+  let currentTime = document.querySelector("#current-time");
+  let date = new Date(response.data.time * 1000);
+  currentTime.innerHTML = fullDate(date);
+  let currentIcon = document.querySelector("#current-temperature-icon");
+  currentIcon.innerHTML = `<img src='${response.data.condition.icon_url}'alt='weather icon'>`;
+}
+function fullDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = `1ffb00o632b81060b5933b1eeta0394d`;
